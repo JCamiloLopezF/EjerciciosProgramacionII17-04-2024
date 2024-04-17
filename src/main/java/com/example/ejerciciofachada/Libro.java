ea32cf40f661111604ee203c1ea92d36b228c05d
Forest <playa200612@gmail.com>
2024-04-17 09:20:09 -0500

    Clase Libro.java

---
+++ src/main/java/com/example/ejerciciofachada/Libro.java
@@ -0,0 +1,42 @@
+package com.example.ejerciciofachada;
+
+public class Libro {
+
+    private String titulo;
+    private String autor;
+    private String categoria;
+
+    public Libro(String titulo, String autor, String categoria) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.categoria = categoria;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String toString() {
+        return titulo + " - " + autor + " - " + categoria;
+    }
+}
